7923049221d83c95a8f9b02db5ab412c19d8a707
KangWon LEE <kangwon.lee@kpu.ac.kr>
2017-07-28 15:19:53 +0700

    remove all outputs and execution_count from all Jupyter notebook files

---
+++ remove_all_outputs_execnt.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+import ipynb_remove_output as ir
+
+
+def main(argv):
+    if 1 < len(argv):
+        # argument # check passed
+
+        # path walk
+        for dir_name, dir_list, filename_list in os.walk(argv[1]):
+            # ignore list
+            if not (('.git' in dir_name) or ('utils3' in dir_name)):
+                # filename loop
+                for filename in filename_list:
+                    if '.ipynb' == os.path.splitext(filename)[-1]:
+                        print(os.path.join(dir_name, filename))
+    else:
+        # pass?
+        ValueError('path not given')
+
+
+if __name__ == '__main__':
+    main(sys.argv)
