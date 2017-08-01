fef1d4e2124f06fef5a73235d62c4e883910c294
KangWon LEE <kangwon.lee@kpu.ac.kr>
2017-08-01 20:31:45 +0900

    main() function

--- 05_Least_Squares/householder_reflection.py
+++ 05_Least_Squares/householder_reflection.py
@@ -1,67 +1,73 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.matrix([[3, 2]]).T
-y = np.matrix([[7, -4]]).T
 
-# find u bisecting x and x axis
-sigma = np.sqrt((x.T * x)[0, 0])
-u = x + sigma * np.matrix([[1, 0]]).T
+def main():
+    x = np.matrix([[3, 2]]).T
+    y = np.matrix([[7, -4]]).T
 
-# Householder reflection
-rho = (2 / (u.T * u))[0, 0]
+    # find u bisecting x and x axis
+    sigma = np.sqrt((x.T * x)[0, 0])
+    u = x + sigma * np.matrix([[1, 0]]).T
 
-tau_x = (rho * u.T * x)[0, 0]
-Hx = x - tau_x * u
+    # Householder reflection
+    rho = (2 / (u.T * u))[0, 0]
 
-tau_y = (rho * u.T * y)[0, 0]
-Hy = y - tau_y * u
+    tau_x = (rho * u.T * x)[0, 0]
+    Hx = x - tau_x * u
 
-# prepare data to draw figure
-x_arrow = [[0, 0], x.T.tolist()[0]]
-y_arrow = [[0, 0], y.T.tolist()[0]]
-u_arrow = [[0, 0], u.T.tolist()[0]]
-u_ortho_arrow = np.array([[-u[1, 0], u[0, 0]],
-                          [u[1, 0], -u[0, 0]]]) * 1.6
-Hx_arrow = [[0, 0], Hx.T.tolist()[0]]
-Hy_arrow = [[0, 0], Hy.T.tolist()[0]]
+    tau_y = (rho * u.T * y)[0, 0]
+    Hy = y - tau_y * u
 
-# prepare axis
-ax = plt.gca()
+    # prepare data to draw figure
+    x_arrow = [[0, 0], x.T.tolist()[0]]
+    y_arrow = [[0, 0], y.T.tolist()[0]]
+    u_arrow = [[0, 0], u.T.tolist()[0]]
+    u_ortho_arrow = np.array([[-u[1, 0], u[0, 0]],
+                              [u[1, 0], -u[0, 0]]]) * 1.6
+    Hx_arrow = [[0, 0], Hx.T.tolist()[0]]
+    Hy_arrow = [[0, 0], Hy.T.tolist()[0]]
 
-# clear axis
-ax.cla()
+    # prepare axis
+    ax = plt.gca()
 
-# draw arrows
-ax.arrow(x_arrow[0][0], x_arrow[0][1], x_arrow[1][0], x_arrow[1][1], head_width=0.5, head_length=0.5)
-ax.arrow(y_arrow[0][0], y_arrow[0][1], y_arrow[1][0], y_arrow[1][1], head_width=0.5, head_length=0.5)
-ax.arrow(u_arrow[0][0], u_arrow[0][1], u_arrow[1][0], u_arrow[1][1], head_width=0.5, head_length=0.5)
-ax.arrow(Hx_arrow[0][0], Hx_arrow[0][1], Hx_arrow[1][0], Hx_arrow[1][1], head_width=0.5, head_length=0.5)
-ax.arrow(Hy_arrow[0][0], Hy_arrow[0][1], Hy_arrow[1][0], Hy_arrow[1][1], head_width=0.5, head_length=0.5)
+    # clear axis
+    ax.cla()
 
-# draw texts
-offset = 0.5
-ax.text(x_arrow[1][0] + offset, x_arrow[1][1], 'x')
-ax.text(y_arrow[1][0] + offset, y_arrow[1][1], 'y')
-ax.text(u_arrow[1][0] + offset, u_arrow[1][1], 'u')
+    # draw arrows
+    ax.arrow(x_arrow[0][0], x_arrow[0][1], x_arrow[1][0], x_arrow[1][1], head_width=0.5, head_length=0.5)
+    ax.arrow(y_arrow[0][0], y_arrow[0][1], y_arrow[1][0], y_arrow[1][1], head_width=0.5, head_length=0.5)
+    ax.arrow(u_arrow[0][0], u_arrow[0][1], u_arrow[1][0], u_arrow[1][1], head_width=0.5, head_length=0.5)
+    ax.arrow(Hx_arrow[0][0], Hx_arrow[0][1], Hx_arrow[1][0], Hx_arrow[1][1], head_width=0.5, head_length=0.5)
+    ax.arrow(Hy_arrow[0][0], Hy_arrow[0][1], Hy_arrow[1][0], Hy_arrow[1][1], head_width=0.5, head_length=0.5)
 
-ax.text(Hx_arrow[1][0] - offset * 3, Hx_arrow[1][1], 'Hx')
-ax.text(Hy_arrow[1][0] - offset * 3, Hy_arrow[1][1], 'Hy')
+    # draw texts
+    offset = 0.5
+    ax.text(x_arrow[1][0] + offset, x_arrow[1][1], 'x')
+    ax.text(y_arrow[1][0] + offset, y_arrow[1][1], 'y')
+    ax.text(u_arrow[1][0] + offset, u_arrow[1][1], 'u')
 
-ax.text(u_ortho_arrow[0, 0] * 0.5, u_ortho_arrow[0, 1] * 0.5, '$u_\perp$')
+    ax.text(Hx_arrow[1][0] - offset * 3, Hx_arrow[1][1], 'Hx')
+    ax.text(Hy_arrow[1][0] - offset * 3, Hy_arrow[1][1], 'Hy')
 
-# draw line orthogonal to u
-plt.plot(u_ortho_arrow[:, 0], u_ortho_arrow[:, 1])
+    ax.text(u_ortho_arrow[0, 0] * 0.5, u_ortho_arrow[0, 1] * 0.5, '$u_\perp$')
 
-# adjust aspect ratio
-plt.axis('equal')
+    # draw line orthogonal to u
+    plt.plot(u_ortho_arrow[:, 0], u_ortho_arrow[:, 1])
 
-# set axis limits
-plt.xlim(-10, 10)
-plt.ylim(-8, 8)
+    # adjust aspect ratio
+    plt.axis('equal')
 
-# draw grid
-plt.grid(True)
+    # set axis limits
+    plt.xlim(-10, 10)
+    plt.ylim(-8, 8)
 
-# present plot
-plt.show()
+    # draw grid
+    plt.grid(True)
+
+    # present plot
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
