192ec62073da0911fa7616b5ce35229d9777d8be
KangWon LEE <kangwon.lee@kpu.ac.kr>
2017-08-06 10:02:27 +0900

    added crypto function example.  # Ref : C. Moler, Numerical computation with MATLAB, SIAM, 2008.

---
+++ 01_intro/crypto.py
@@ -0,0 +1,74 @@
+import numpy as np
+
+
+def crypto(x_str):
+    """
+    CRYPTO Cryptography example.
+    y = crypto(x) converts an ASCII text string into another, coded string.
+    The function is its own inverse, so crypto(crypto(x)) gives x back.
+    See also: ENCRYPT.
+
+      Copyright 2014 Cleve Moler
+      Copyright 2014 The MathWorks, Inc.
+
+    Ref : C. Moler, Numerical computation with MATLAB, SIAM, 2008.
+    """
+    x_char_array = np.array(tuple(x_str), dtype='|U1')
+
+    # Use a two-character Hill cipher with arithmetic modulo 97, a prime.
+    p = 97
+
+    # Choose two characters above ASCII 128 to expand set from 95 to 97.
+    c1 = chr(169)
+    c2 = chr(174)
+    x_char_array[x_char_array == c1] = 127
+    x_char_array[x_char_array == c2] = 128
+
+    # Convert to integers mod p.
+    x_int_array = np.mod((x_char_array.view(np.int8) - 32), p)
+
+    # Reshape into a matrix with 2 rows and floor(length(x)/2) columns.
+    n = int(2 * np.floor(len(x_int_array) / 2))
+    x_int_mat = np.matrix(np.reshape(x_int_array[0:n], (2, n // 2)))
+
+    # Encode with matrix multiplication modulo p.
+    mat_a_int = np.matrix([[71, 2],
+                           [2, 26]], dtype=int)
+
+    mat_y = np.mod(mat_a_int * x_int_mat, p)
+
+    # Reshape into a single row.
+    y_int_list = np.reshape(mat_y, (1, n)).tolist()[0]
+
+    # If length(x) is odd, encode the last character.
+    if len(x_int_array) > n:
+        y_int_list.append(np.mod((p - 1) * x_int_array[-1], p))
+
+    # Convert to ASCII characters.
+    result = convert_to_ascii(c1, c2, y_int_list)
+
+    return result
+
+
+def convert_to_ascii(c1, c2, y_int_list):
+    y_int_array = np.array(y_int_list, dtype=np.uint8)
+    # y_int_array += 32
+
+    y_int_no_offset_list = [y_int_offset + 32 for y_int_offset in y_int_list]
+
+    # y_char_array = y_int_array.astype(np.uint8).view(dtype='U1')
+    # y_char_array[y_char_array == chr(127)] = c1
+    # y_char_array[y_char_array == chr(128)] = c2
+    y_char_recover_127_list = []
+
+    for y_int in y_int_no_offset_list:
+        append_this = chr(y_int)
+        if chr(127) == append_this:
+            append_this = c1
+        elif chr(128) == append_this:
+            append_this = c2
+        y_char_recover_127_list.append(append_this)
+
+    result = ''.join(y_char_recover_127_list)
+
+    return result
---
+++ 01_intro/test_crypto.py
@@ -0,0 +1,17 @@
+import unittest
+
+import crypto
+
+
+class TestCrypto(unittest.TestCase):
+    def test_crypto(self):
+        input_str = ''.join([chr(i) for i in range(32, (95+32+1))])
+
+        encrypted = crypto.crypto(input_str)
+        expected_for_now = '''®yyyhyyyPyyy8yyy yyyiyyyQyyy9yyy!yyyjyyyRyyy:yyy"yyykyyySyyy;yyy#yyylyyyTyyy<yyy$yyymyyyUyyy=yyy%yyynyyyVyyy>yyy&yyyoyyyWyyy?yyy'yyypyyyXyyy@yyy(yyyqyyyYyyyAyyy)yyyryyyZyyyByyy*yyysyyy[yyyCyyytjjj/jjjKjjjgjjj"jjj>jjjZjjjvjjj1jjjMjjjijjj$jjj@jjj\jjjxjjj3jjjOjjjkjjj&jjjBjjj^jjjzjjj5jjjQjjjmjjj(jjjDjjj`jjj|jjj7jjjSjjjojjj*jjjFjjjbjjj~jjj9jjjUjjjqjjj,jjjHjjjdjjj®jjj;jjjWjjjsjjj.jjjJjjj'''
+
+        self.assertEqual(expected_for_now, encrypted)
+
+
+if __name__ == '__main__':
+    unittest.main()
