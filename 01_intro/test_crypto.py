34320adc8d6f998609406a4444101bca2f66115b
KangWon LEE <kangwon.lee@kpu.ac.kr>
2017-08-06 10:42:59 +0900

    started test_convert_2_int_array()

--- 01_intro/test_crypto.py
+++ 01_intro/test_crypto.py
@@ -16,6 +16,18 @@ class TestCrypto(unittest.TestCase):
 
         self.assertEqual(input_str, decrypted)
 
+    def test_convert_2_int_array(self):
+        p = 97
+        c1 = chr(169)
+        c2 = chr(174)
+        v1 = 127
+        v2 = 128
+
+        input_str = 'TV'
+        x_int_array = crypto.convert_2_int_array(input_str, c1, v1, c2, v2, p)
+
+        self.assertEqual(len(input_str), len(x_int_array))
+
 
 if __name__ == '__main__':
     unittest.main()
